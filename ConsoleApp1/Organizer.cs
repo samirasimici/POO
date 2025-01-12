a7bb2c2914797b5fbce59b396787b77df6327f50
sebi77777 <andrei.pruna@student.upt.ro>
2025-01-13 00:58:53 +0200

    Crearea clasei pentru organizatori + adaugarea unui eveniment

---
+++ ConsoleApp1/Organizer.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1;
+
+public class Organizer : User
+{
+    private EventManager eventManager;
+
+    public Organizer(EventManager eventManager)
+    {
+        this.eventManager = eventManager;
+    }
+
+    public void LansareEveniment(string nume, string descriere, int capacitate, DateTime data)
+    {
+        Event nouEveniment = new Event
+        {
+            Id = eventManager.GetNextEventId(),
+            Nume = nume,
+            Descriere = descriere,
+            Capacitate = capacitate,
+            Data = data
+        };
+        eventManager.AdaugareEveniment(nouEveniment);
+        Console.WriteLine($"Evenimentul {nume} a fost lansat cu succes.");
+    }
+}
