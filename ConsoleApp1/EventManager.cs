29e865c79a85f2960698455045f14cbde1537494
Andrei <andreialdulescu@yahoo.com>
2025-01-12 23:50:32 +0200

    1.	Clasa User: Aceasta este clasa de bază care reprezintă un utilizator generic. Are proprietăți comune pentru toți utilizatorii: Id, Nume, Prenume, Email și Parola. De asemenea, are o metodă virtuală Meniu care va fi suprascrisă.
    Clasa EventManager:
    	Aceasta este clasa care gestionează toate evenimentele din aplicație. EventManager are o listă de evenimente și metode pentru a crea, căuta și afișa evenimentele. Este folosită pentru gestionarea evenimentelor atât de organizatori, cât și de clienți.

---
+++ ConsoleApp1/EventManager.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp1;
+
+public class EventManager
+{
+    private List<Event> evenimente = new List<Event>();
+    private List<Client> clienti = new List<Client>();
+
+    public int GetNextEventId()
+    {
+        return evenimente.Count + 1;
+    }
+
+    public void AdaugareEveniment(Event eveniment)
+    {
+        evenimente.Add(eveniment);
+    }
+
+    public Event GasesteEveniment(int id)
+    {
+        return evenimente.FirstOrDefault(e=> e.Id == id);
+    }
+
+    public List<Event> GetAllEvenimente()
+    {
+        return evenimente;
+    }
+
+    public Client GasesteClient(int id)
+    {
+        return clienti.FirstOrDefault(c => c.Id == id);
+    }
+
+    public void AdaugaClient(Client client)
+    {
+        clienti.Add(client);
+    }
+}
