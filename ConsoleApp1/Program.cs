4dcf90428ed427731b9f2438e805b1daa576cb0d
sebi77777 <andrei.pruna@student.upt.ro>
2025-01-13 19:10:42 +0200

    am adus modificari la functionalitatea meniului

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
         EventManager globalEventManager = new EventManager();
+        
         Organizer organizator = new Organizer(globalEventManager)
         {
             Id = 1,
@@ -13,6 +14,7 @@ public class Program
             Email = "andreialdulescu@yahoo.com",
             Parola = "Andrei123"
         };
+
         Client client1 = new Client(globalEventManager)
         {
             Id = 2,
@@ -21,14 +23,21 @@ public class Program
             Email = "samirasimici@yahoo.com",
             Parola = "Samira456"
         };
-
+        
         Client client2 = new Client(globalEventManager)
         {
             Id = 3,
-            Nume = "Andrei",
-            Prenume = "Pruna",
-            Email = "andreipruna@yahoo.com",
-            Parola = "Pruna789"
+            Nume = "Pruna",
+            Prenume = "Sebi",
+            Email = "prunasebi@yahoo.com",
+            Parola = "Sebi789"
         };
+        
+        App app = new App(globalEventManager);
+        app.AdaugaUtilizator(organizator);
+        app.AdaugaUtilizator(client1);
+        app.AdaugaUtilizator(client2);
+        
+        app.Start();
     }
 }
